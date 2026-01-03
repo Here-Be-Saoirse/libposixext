c5e474ec98d87841fc8fcd02c3aee70a24753788
Freya Fractal <iosrokz001@gmail.com>
2026-01-03 00:02:48 -0800

    Update posixext.c: update license

--- posixext.c
+++ posixext.c
@@ -1,3 +1,31 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://illumos.org/license/CDDL.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+
+/*
+ * Copyright 2025 Fractal Microsystems, Inc.  All rights reserved.
+ * Use is subject to license terms.
+ */
+
+
 #include "posixext.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -253,4 +281,5 @@ FILE *fmemopen(void *buf, size_t size, const char *mode) {
     }
 
     return funopen(memfile, memfile_read, memfile_write, memfile_seek, memfile_close);
+
 }
